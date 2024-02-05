19bbe3cc0545ee576291ee68f0c12f589be3fb58
Asadell <m.satrioasadell@gmail.com>
2024-02-05 18:15:10 +0700

    error handling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const app = express();
+const ErrorHandler = require('./ErrorHandler');
 
 // Models
 const Product = require('./models/product');
@@ -22,53 +23,102 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+function wrapAsync(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((err) => next(err));
+  };
+}
+
 // Route Path
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/products', async (req, res) => {
-  const { category } = req.query;
-  if (category) {
-    const products = await Product.find({ category });
-    res.render('products/index', { products, category });
-  } else {
-    const products = await Product.find({});
-    res.render('products/index', { products, category: 'All' });
-  }
-});
+app.get(
+  '/products',
+  wrapAsync(async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+      const products = await Product.find({ category });
+      res.render('products/index', { products, category });
+    } else {
+      const products = await Product.find({});
+      res.render('products/index', { products, category: 'All' });
+    }
+  })
+);
 
 app.get('/products/create', (req, res) => {
+  // throw new ErrorHandler('This is a custom error', 503);
   res.render('products/create');
 });
 
-app.post('/products', async (req, res) => {
-  const product = new Product(req.body);
-  await product.save();
-  res.redirect('/products');
+app.post(
+  '/products',
+  wrapAsync(async (req, res) => {
+    const product = new Product(req.body);
+    await product.save();
+    res.redirect('/products');
+  })
+);
+
+app.get(
+  '/products/:id',
+  wrapAsync(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.render('products/show', { product });
+  })
+);
+
+app.put(
+  '/products/:id',
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+    });
+    res.redirect(`/products/${product._id}`);
+  })
+);
+
+app.get(
+  '/products/:id/edit',
+  wrapAsync(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.render('products/edit', { product });
+  })
+);
+
+app.delete(
+  '/products/:id',
+  wrapAsync(async (req, res) => {
+    await Product.findByIdAndDelete(req.params.id);
+    res.redirect('/products');
+  })
+);
+
+const validationHandler = (err) => {
+  err.status = 400;
+  err.message = Object.values(err.errors).map((item) => item.message);
+  return new ErrorHandler(err.message, err.status);
+};
+
+app.use((err, req, res, next) => {
+  console.dir(err);
+  if (err.name === 'ValidationError') {
+    err.status = 400;
+    err.message = Object.values(err.errors).map((item) => item.message);
+  }
+  if (err.name === 'CastError') {
+    err.status = 404;
+    err.message = 'Product not found';
+  }
+  next(err);
 });
 
-app.get('/products/:id', async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.render('products/show', { product });
-});
-
-app.put('/products/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, req.body, {
-    runValidators: true,
-  });
-  res.redirect(`/products/${product._id}`);
-});
-
-app.get('/products/:id/edit', async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.render('products/edit', { product });
-});
-
-app.delete('/products/:id', async (req, res) => {
-  await Product.findByIdAndDelete(req.params.id);
-  res.redirect('/products');
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Something went wrong' } = err;
+  res.status(status).send(message);
 });
 
 app.listen(3000, () => {
